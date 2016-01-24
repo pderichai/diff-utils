c8b18f95181334a8c5b92c106838b077166da45f
Deric Pang <pderichai@gmail.com>
2016-01-24 01:21:21 -0800

    Changed the way files are read.

--- src/edu/washington/cs/dericp/diffutils/Utils.java
+++ src/edu/washington/cs/dericp/diffutils/Utils.java
@@ -1,18 +1,22 @@
 package edu.washington.cs.dericp.diffutils;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+
 
 /**
  * Utility functions that are used throughout the diff utility.
  */
-public class Utils {
+class Utils {
     
     /**
      * Converts a File to a List of Strings.
@@ -20,19 +24,15 @@ public class Utils {
      * @param filePath      a String, the path of the file on the machine
      * @return              a List of Strings, containing the information of the file
      *                      at the designated file-path
+     * @throws IOException 
      */
-    public static List<String> fileToLines(String filePath) {
+    public static List<String> fileToLines(String relFilePath) throws IOException {
         List<String> lines = new ArrayList<String>();
-        String line = "";
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(filePath));
-            while ((line = in.readLine()) != null) {
-                lines.add(line);
-            }
-            in.close();
-        } catch (IOException e) {
-                e.printStackTrace();
+        BufferedReader reader = new BufferedReader(new FileReader(relFilePath));
+        while (reader.ready()) {
+            lines.add(reader.readLine());
         }
+        reader.close();
         return lines;
     }
     
@@ -42,20 +42,18 @@ public class Utils {
      * @param fileLines     a List of Strings, contains the information to be output in the file
      * @param filePath      a String, the path of the file on the machine
      * @return
+     * @throws IOException 
      */
-    public static File linesToFile(List<String> fileLines, String filePath) {
-        File file = new File(filePath);
-        try {
-            PrintStream output = new PrintStream(file);
-            for (String line : fileLines) {
-                if (line != null) {
-                    output.println(line);
-                }
-            }
-            output.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+    public static void linesToFile(List<String> fileLines, String relFilePath) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(relFilePath));
+        Iterator<String> lineIter = fileLines.iterator();
+        if (lineIter.hasNext()) {
+            writer.write(lineIter.next());
         }
-        return file;
+        while (lineIter.hasNext()) {
+            writer.newLine();
+            writer.write(lineIter.next());
+        }
+        writer.close();
     }
 }
